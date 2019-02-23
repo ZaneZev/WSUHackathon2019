da06cafdc7885a57b09c212f73faa6f1896cda7e
Jimmy Zheng <jimmyz0622@gmail.com>
2019-02-23 13:49:10 -0800

    herk

--- app/src/main/java/com/example/deadline/MainActivity.java
+++ app/src/main/java/com/example/deadline/MainActivity.java
@@ -80,13 +80,13 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.nav_today) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_goal) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_skill) {
+
+        } else if (id == R.id.nav_setting) {
 
         } else if (id == R.id.nav_share) {
 
